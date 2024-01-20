d40010415b53cc5ce4a5b5d04fc947f62fd723d9
pig8pig <supasupaweiwei@gmail.com>
2024-01-19 17:39:58 -0800

    changed auto
    auto shows team prop position before starting

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoTestRed.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoTestRed.java
@@ -21,13 +21,19 @@ public class AutoTestRed extends LinearOpMode {
         odometryBot.init(hardwareMap);
         ColorDetectionBot colorDetectionBot = new ColorDetectionBot(this);
         controlHubCam = colorDetectionBot.initOpenCV(hardwareMap, controlHubCam, true);
+        int position = colorDetectionBot.detect(); //call method from ColorDetection to know left/middle/right
+
+        while (!opModeIsActive()) {
+            position = colorDetectionBot.detect();
+            telemetry.addData("Position of pixel", position);
+            telemetry.update();
+        }
 
         waitForStart();
         FtcDashboard dashboard = FtcDashboard.getInstance();
         telemetry = new MultipleTelemetry(telemetry, dashboard.getTelemetry());
         FtcDashboard.getInstance().startCameraStream(controlHubCam, 30);
 
-        int position = colorDetectionBot.detect(); //call method from ColorDetection to know left/middle/right
         telemetry.addData("Position of pixel", position);
         telemetry.update();
 
